b189babb00025ec9f7eeee27a1d2dca57019a135
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-11-18 22:34:28 -0500

    connects to mongo and has book model

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const connectDB =require('./config/db');
+
+const app = express();
+
+connectDB();
+
+
+// init middleware
+app.use(express.json({extended:false}))
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('api working')
 })
+
+const PORT = process.env.PORT || 5000;
  
-app.listen(5000)
+app.listen(PORT, ()=>{console.log(`Server running on ${PORT}`)})
