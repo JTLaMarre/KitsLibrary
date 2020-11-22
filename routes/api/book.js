e9ff08b930fae41bcd094edb93fa225982b008ab
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-11-22 13:02:19 -0500

    get all books route works

--- routes/api/book.js
+++ routes/api/book.js
@@ -6,7 +6,38 @@ const config = require('config');
 const Book = require('../../models/Book');
 
 // @route /book
-// @desc posts a book 
-router.get('/',(req,res)=>res.send('Books Route'))
+// @desc getss all book 
+router.get('/',async(req,res)=>{
+try{
+const books = await Book.find().sort();
+res.json(books);
+}catch(err){
+    console.error(err.message);
+    res.status(500).send('server error');
+}
+})
+
+// @route /book
+// @desc post a book
+router.post('/', async(req,res)=>{
+
+
+
+try{
+   const NewBook = new Book({
+       Title: req.body.Title,
+       Author:req.body.Author,
+       BookType:req.body.BookType
+   })    
+
+   const book = await NewBook.save();
+
+   res.json(book);
+}catch(err){
+console.error(err.message)
+res.status(500).send('Server Error');
+}
+
+})
 
 module.exports = router;
