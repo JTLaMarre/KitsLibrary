0e960e6769efee3662d0d75ffe3af4f2b8eb7dbe
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-11-25 20:02:07 -0500

    a little styling

--- client/src/App.js
+++ client/src/App.js
@@ -9,22 +9,23 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 
 function App() {
 
   const [books, setBooks] = useState([])
-  const [title, setTitle]=useState('');
-  const [author, setAuthor]=useState('');
-  const[bookType, setBookType]=useState('');
-  const[search, setSearch]=useState('');
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [bookType, setBookType] = useState('');
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     handleBooks();
   }, [])
 
   const handleBooks = async () => {
-     await axios.get('/api/book')
+    await axios.get('/api/book')
       .then((res => {
 
         console.log(res.data)
@@ -41,56 +42,61 @@ function App() {
     console.log('Removed!!')
     handleBooks();
   }
-  const handleSubmit = ()=>{
+  const handleSubmit = () => {
     console.log(title, author, bookType)
     axios({
-      method:'post',
-      url:'api/book',
-      data:{
-        Title:title,
-        Author:author,
-        BookType:bookType
+      method: 'post',
+      url: 'api/book',
+      data: {
+        Title: title,
+        Author: author,
+        BookType: bookType
       }
     })
     handleBooks();
   }
 
-  const searchByTitle= async ()=>{
+  const searchByTitle = async () => {
     console.log(search)
-    await axios.get('/api/book/'+search)
-    .then((res)=>{
-      console.log(res)
-      console.log(res.data)
-      if(res.data===null){
-        alert("you don't own this book")
-      }
-      else{
-        alert(`you own, ${res.data.Title}`)
-      }
-      
+    await axios.get('/api/book/' + search)
+      .then((res) => {
+        console.log(res)
+        console.log(res.data)
+        if (res.data === null) {
+          alert("you don't own this book")
+        }
+        else {
+          alert(`you own, ${res.data.Title}`)
+        }
 
-    })
+
+      })
   }
 
 
   return (
     <Container>
-      <Row>
+      <Jumbotron fluid id='jumbo' >
+        <Container >
+          <h1 id='bar'>Home Library</h1>
+        </Container>
+      </Jumbotron>
+      <Row md={4}>
         <Col>
-        <InputGroup className="mb-3">
-    <InputGroup.Prepend>
-      <Button variant="outline-secondary" onClick={searchByTitle}>Search</Button>
-    </InputGroup.Prepend>
-    <FormControl aria-describedby="basic-addon1" placeholder="Search for books here" value={search} onChange={e=>setSearch(e.target.value)} />
-  </InputGroup>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <Button variant="outline-secondary" onClick={searchByTitle}>Search</Button>
+            </InputGroup.Prepend>
+            <FormControl aria-describedby="basic-addon1" placeholder="Search for books here" value={search} onChange={e => setSearch(e.target.value)} />
+          </InputGroup>
         </Col>
       </Row>
-      <Row>
+      <Row md={4}>
         <Col>
           <Form>
             <Form.Group controlId="Title">
               <Form.Label>Title</Form.Label>
-              <Form.Control type="text" placeholder="Enter Title" value={title} onChange={e => setTitle(e.target.value)}/>
+              <Form.Control type="text" placeholder="Enter Title" value={title} onChange={e => setTitle(e.target.value)} />
             </Form.Group>
             <Form.Group controlId="Author">
               <Form.Label>Author</Form.Label>
@@ -110,9 +116,15 @@ function App() {
 
       {books.map((book) => {
         return (
-          <Row>
+          <Row md={4}>
             <Col>
-              <p>|Title:{book.Title}| |Author:{book.Author}|   |{book.BookType}|</p>
+              <p>|Title:{book.Title}</p>
+            </Col>
+            <Col>
+              <p>|Author:{book.Author}</p>
+            </Col>
+            <Col>
+              <p> |{book.BookType}</p>
             </Col>
             <Col>
               <button className="btn btn-danger" onClick={() => removeBook(book._id)}>delete</button>
